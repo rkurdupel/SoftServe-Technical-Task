7d1206ac3e25737eee54a4322303fd21faf816b6
Roman Kurdupel <rkurdupel@Romans-MacBook-Pro.local>
2026-06-09 21:11:28 +0300

    feat: add ssh tomcat and mysql nsg security rules

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,6 +34,29 @@ resource "azurerm_network_security_group" "eschool" {
         destination_address_prefix = "*"
     }
 
+    security_rule {
+        name                       = "allow-mysql"
+        priority                   = 110
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "3306"
+        source_address_prefix      = "10.0.1.0/24"
+        destination_address_prefix = "*"
+    }
+
+    security_rule {
+        name                       = "allow-tomcat"
+        priority                   = 120
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "8080"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
 }
 
 resource "azurerm_public_ip" "eschool" {
