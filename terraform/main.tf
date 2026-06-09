0d43fe37dd962399961c55c46b61a146e1fa9718
Roman Kurdupel <rkurdupel@Romans-MacBook-Pro.local>
2026-06-09 19:36:57 +0300

    fix: add sh inboundn rule to nsg

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,6 +22,18 @@ resource "azurerm_network_security_group" "eschool" {
     resource_group_name = azurerm_resource_group.eschool.name
     location = azurerm_resource_group.eschool.location
 
+    security_rule {
+        name                       = "allow-ssh"
+        priority                   = 100
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
 }
 
 resource "azurerm_public_ip" "eschool" {
