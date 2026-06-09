6f2e1d3058b6c711228f6d5872a8a773b47a13ef
Roman Kurdupel <rkurdupel@Romans-MacBook-Pro.local>
2026-06-09 22:16:34 +0300

    chore: fix terraform formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,117 +1,117 @@
 resource "azurerm_resource_group" "eschool" {
-    name = "${var.name_prefix}-rg"
-    location = var.location
+  name     = "${var.name_prefix}-rg"
+  location = var.location
 }
 
 resource "azurerm_virtual_network" "eschool" {
-    name = "${var.name_prefix}-virtual-network"
-    resource_group_name = azurerm_resource_group.eschool.name
-    location = azurerm_resource_group.eschool.location
-    address_space = ["10.0.0.0/16"]
+  name                = "${var.name_prefix}-virtual-network"
+  resource_group_name = azurerm_resource_group.eschool.name
+  location            = azurerm_resource_group.eschool.location
+  address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "eschool" {
-    name = "${var.name_prefix}-subnet"
-    resource_group_name = azurerm_resource_group.eschool.name
-    virtual_network_name = azurerm_virtual_network.eschool.name
-    address_prefixes = ["10.0.1.0/24"]
+  name                 = "${var.name_prefix}-subnet"
+  resource_group_name  = azurerm_resource_group.eschool.name
+  virtual_network_name = azurerm_virtual_network.eschool.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_security_group" "eschool" {
-    name = "${var.name_prefix}-network-sg"
-    resource_group_name = azurerm_resource_group.eschool.name
-    location = azurerm_resource_group.eschool.location
+  name                = "${var.name_prefix}-network-sg"
+  resource_group_name = azurerm_resource_group.eschool.name
+  location            = azurerm_resource_group.eschool.location
 
-    security_rule {
-        name                       = "allow-ssh"
-        priority                   = 100
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "22"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
+  security_rule {
+    name                       = "allow-ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
-    security_rule {
-        name                       = "allow-mysql"
-        priority                   = 110
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "3306"
-        source_address_prefix      = "10.0.1.0/24"
-        destination_address_prefix = "*"
-    }
+  security_rule {
+    name                       = "allow-mysql"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3306"
+    source_address_prefix      = "10.0.1.0/24"
+    destination_address_prefix = "*"
+  }
 
-    security_rule {
-        name                       = "allow-tomcat"
-        priority                   = 120
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "8080"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
+  security_rule {
+    name                       = "allow-tomcat"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_public_ip" "eschool" {
-    count = var.vm_count
-    name = "${var.name_prefix}-vm${count.index + 1}-publicip"
-    resource_group_name = azurerm_resource_group.eschool.name
-    location = azurerm_resource_group.eschool.location
-    allocation_method = "Static"
-   
+  count               = var.vm_count
+  name                = "${var.name_prefix}-vm${count.index + 1}-publicip"
+  resource_group_name = azurerm_resource_group.eschool.name
+  location            = azurerm_resource_group.eschool.location
+  allocation_method   = "Static"
+
 }
 
 resource "azurerm_network_interface" "eschool" {
-    count = var.vm_count
-    name = "${var.name_prefix}-vm${count.index + 1}-network-ic"
-    resource_group_name = azurerm_resource_group.eschool.name
-    location = azurerm_resource_group.eschool.location
+  count               = var.vm_count
+  name                = "${var.name_prefix}-vm${count.index + 1}-network-ic"
+  resource_group_name = azurerm_resource_group.eschool.name
+  location            = azurerm_resource_group.eschool.location
 
-    ip_configuration {
-        name = "${var.name_prefix}-vm${count.index + 1}-ip-config"
-        subnet_id = azurerm_subnet.eschool.id
-        private_ip_address_allocation = "Dynamic"
-        public_ip_address_id = azurerm_public_ip.eschool[count.index].id
-    }
+  ip_configuration {
+    name                          = "${var.name_prefix}-vm${count.index + 1}-ip-config"
+    subnet_id                     = azurerm_subnet.eschool.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.eschool[count.index].id
+  }
 }
 
 resource "azurerm_network_interface_security_group_association" "eschool" {
-    count = var.vm_count
-    network_interface_id = azurerm_network_interface.eschool[count.index].id
-    network_security_group_id = azurerm_network_security_group.eschool.id
+  count                     = var.vm_count
+  network_interface_id      = azurerm_network_interface.eschool[count.index].id
+  network_security_group_id = azurerm_network_security_group.eschool.id
 }
 
 
 resource "azurerm_linux_virtual_machine" "vm" {
-    count = var.vm_count
-    name = "${var.name_prefix}-vm${count.index + 1}"
-    resource_group_name = azurerm_resource_group.eschool.name
-    location = azurerm_resource_group.eschool.location
-    size = var.vm_size
-    admin_username = var.admin_username
-    network_interface_ids = [azurerm_network_interface.eschool[count.index].id]
+  count                 = var.vm_count
+  name                  = "${var.name_prefix}-vm${count.index + 1}"
+  resource_group_name   = azurerm_resource_group.eschool.name
+  location              = azurerm_resource_group.eschool.location
+  size                  = var.vm_size
+  admin_username        = var.admin_username
+  network_interface_ids = [azurerm_network_interface.eschool[count.index].id]
 
-    admin_ssh_key {
-        username = var.admin_username
-        public_key = file(pathexpand(var.ssh_public_key_path))
-    }
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(pathexpand(var.ssh_public_key_path))
+  }
 
-    os_disk {
-        caching = "ReadWrite"
-        storage_account_type = "Standard_LRS"
-    }
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 
-    source_image_reference {
-        publisher = "Canonical"
-        offer = "0001-com-ubuntu-server-jammy"
-        sku = "22_04-lts"
-        version = "latest"
-    }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,29 +1,29 @@
 variable "name_prefix" {
-    type = string
-    default = "eschool"
+  type    = string
+  default = "eschool"
 }
 
 variable "location" {
-    type = string
-    default = "denmarkeast"
+  type    = string
+  default = "denmarkeast"
 }
 
 variable "admin_username" {
-    type = string
-    default = "azureuser"
+  type    = string
+  default = "azureuser"
 }
 
 variable "ssh_public_key_path" {
-    type = string
-    default = "~/.ssh/id_rsa.pub"
+  type    = string
+  default = "~/.ssh/id_rsa.pub"
 }
 
 variable "vm_size" {
-    type = string
-    default = "Standard_B2s"
+  type    = string
+  default = "Standard_B2s"
 }
 
 variable "vm_count" {
-    type = number
-    default = 2
+  type    = number
+  default = 2
 }
