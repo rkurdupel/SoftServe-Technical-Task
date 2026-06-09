96add95e677cbaded3cdd8f0b696f0fb0d36704b
Roman Kurdupel <rkurdupel@Romans-MacBook-Pro.local>
2026-06-09 19:30:15 +0300

    feat: add terraform infrastructure deployment

---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "name_prefix" {
+    type = string
+    default = "eschool"
+}
+
+variable "location" {
+    type = string
+    default = "denmarkeast"
+}
+
+variable "admin_username" {
+    type = string
+    default = "azureuser"
+}
+
+variable "ssh_public_key_path" {
+    type = string
+    default = "~/.ssh/id_rsa.pub"
+}
+
+variable "vm_size" {
+    type = string
+    default = "Standard_B2s"
+}
+
+variable "vm_count" {
+    type = string
+    default = 2
+}
